1151749d6e2df70e6c47e991b8f5d7c735270ba0
Mouad Elmoudni <elmoudni.mouad@gmail.com>
2019-05-27 18:56:13 +0200

    rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,45 @@
 class RentalsController < ApplicationController
+
+  def index
+    @rentals = Rental.all
+  end
+
+  def new
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @rental = Rental.new
+  end
+
+  def create
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    # @user = User.find(params[:user_id])
+    @rental = Rental.new(rental_params)
+    @rental.pokemon = @pokemon
+    @rental.user = @user
+    p "-"*10
+    p @rental
+    if @rental.save
+      redirect_to rental_path(@rental)
+    else
+      redirect_to pokemon_path(@pokemon)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:pokemon_id, :user_id, :starting_date, :ending_date)
+  end
+
 end
