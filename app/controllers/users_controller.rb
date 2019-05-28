8fd983a214de23b37875a292cac0e3d3bb4c8182
Alex Reed <ch.alexandre.reed@gmail.com>
2019-05-28 11:23:33 +0200

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def show
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to pokemons_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    @user.update(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @user.destroy
-  end
-
-  private
-
-  def set_user
     @user = User.find(params[:id])
   end
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :adress, :photo)
-  end
-
 end
