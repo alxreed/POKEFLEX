643b4d3472716ff80404c2db29e591771dc7be54
Mouad Elmoudni <elmoudni.mouad@gmail.com>
2019-05-29 16:06:45 +0200

    moult changes 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pokemons#index'
+  resources :users
+  resources :rentals, except: [:new, :create, :destroy]
   resources :pokemons do
     resources :reviews
-    resources :rentals
+    resources :rentals, only: [:new, :create]
   end
-  get '/users/:id', to: 'users#show', as: :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
