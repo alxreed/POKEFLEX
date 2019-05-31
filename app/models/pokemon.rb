04bfc9888191faa5101421067346cf24a91a71a4
Alex Reed <ch.alexandre.reed@gmail.com>
2019-05-31 11:30:27 +0200

    migration pokemon et form

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -6,6 +6,12 @@ class Pokemon < ApplicationRecord
 
   validates :name, presence: true, allow_blank: false
   validates :level, presence: true, inclusion: { in: (1..100) }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   validates :race, presence: true
+  validates :hp, presence: true, numericality: { greater_than: 0 }
+  validates :attack, presence: true, numericality: { greater_than: 0 }
+  validates :defense, presence: true, numericality: { greater_than: 0 }
+  validates :special_attack, presence: true, numericality: { greater_than: 0 }
+  validates :special_defense, presence: true, numericality: { greater_than: 0 }
+  validates :speed, presence: true, numericality: { greater_than: 0 }
 end
